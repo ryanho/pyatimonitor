9350247381c5f23954abcbf6f151df9750c6bccf
Ryan Ho <none@none>
2010-02-21 14:02:20 +0800

    minor UI change, make this widget suit user's theme

--- contents/code/main.py
+++ contents/code/main.py
@@ -4,12 +4,11 @@
     using ATI catalyst driver to monitor core clocks, memory clocks,
     gpu usage and core temperature.
     
-    Copyright (C) 2009  Ryan Ho <koungho@gmail.com>
+    Copyright (C) 2009 Ryan Ho <koungho@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+    it under the terms of the GNU General Public License version 2 as 
+    published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -27,13 +26,24 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
+#from PyKDE4.kdecore import *
 
 class ATImon(plasmascript.Applet):
     def __init__(self,parent,args=None):
         plasmascript.Applet.__init__(self,parent)
  
     def init(self):
-        #self.resize(-1,-1)
+        self.connect(Plasma.Theme.defaultTheme(), SIGNAL("themeChanged()"), self.themeChanged)
+        
+        #load value from default theme, make this widget suit it
+        theme = Plasma.Theme.defaultTheme()
+        t_textcolor = theme.color(Plasma.Theme.TextColor)
+        
+        #load Kconfig
+        #cg = KConfigGroup.config()
+        #d_refreshtime = cg.readEntry("refreshtime", 2)
+        
+        #self.resize(300, 200)
         self.setHasConfigurationInterface(False)
         self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
  
@@ -50,12 +60,14 @@ class ATImon(plasmascript.Applet):
         self.layout = QGraphicsLinearLayout(Qt.Vertical, self.applet)
         self.cardname = Plasma.Label(self.applet)
         self.cardname.setText(result[6])
+        self.cardname.setAlignment(Qt.AlignHCenter)
         #self.cardname.setAlignment()
         self.meter1 = Plasma.Meter(self.applet)
         self.meter1.setMeterType(Plasma.Meter.BarMeterHorizontal)
         self.meter1.setLabel(0,'GPU load')
         self.meter1.setLabel(1, result[2] + '%')
-        self.meter1.setLabelFont(0,QFont('Sans',8))
+        self.meter1.setLabelFont(0, QFont('Sans',8))
+        self.meter1.setLabelColor(0, t_textcolor)
         self.meter1.setMinimum(0)
         self.meter1.setMaximum(100)
         self.meter1.setValue(int(result[2]))
@@ -63,22 +75,25 @@ class ATImon(plasmascript.Applet):
         self.meter2.setMeterType(Plasma.Meter.BarMeterHorizontal)
         self.meter2.setLabel(1,result[3] + u'℃')
         self.meter2.setLabel(0,'GPU temp.')
-        self.meter2.setLabelFont(0,QFont('Sans',8))
+        self.meter2.setLabelFont(0, QFont('Sans',8))
+        self.meter2.setLabelColor(0, t_textcolor)
         self.meter2.setMinimum(0)
         self.meter2.setMaximum(110)
         self.meter2.setValue(int(result[3]))
         self.meter3 = Plasma.Meter(self.applet)
         self.meter3.setMeterType(Plasma.Meter.BarMeterHorizontal)
-        self.meter3.setLabel(1,result[0] + 'MHz')
-        self.meter3.setLabel(0,'Core Clock')
+        self.meter3.setLabel(1, result[0] + 'MHz')
+        self.meter3.setLabel(0, 'Core Clock')
         self.meter3.setLabelFont(0,QFont('Sans',8))
+        self.meter3.setLabelColor(0,t_textcolor)
         self.meter3.setMaximum(int(result[4]))
         self.meter3.setValue(int(result[0]))
         self.meter4 = Plasma.Meter(self.applet)
         self.meter4.setMeterType(Plasma.Meter.BarMeterHorizontal)
-        self.meter4.setLabel(1,result[1] + 'MHz')
-        self.meter4.setLabel(0,'Memory Clock')
+        self.meter4.setLabel(1, result[1] + 'MHz')
+        self.meter4.setLabel(0, 'Memory Clock')
         self.meter4.setLabelFont(0,QFont('Sans',8))
+        self.meter4.setLabelColor(0,t_textcolor)
         self.meter4.setMaximum(int(result[5]))
         self.meter4.setValue(int(result[1]))
         self.layout.addItem(self.cardname)
@@ -125,6 +140,14 @@ class ATImon(plasmascript.Applet):
         self.meter3.setValue(int(result[0]))
         self.meter4.setLabel(1,result[1] + 'MHz')
         self.meter4.setValue(int(result[1]))
+        
+    def themeChanged(self):
+        theme = Plasma.Theme.defaultTheme()
+        t_textcolor = theme.color(Plasma.Theme.TextColor)
+        self.meter1.setLabelColor(0, t_textcolor)
+        self.meter2.setLabelColor(0, t_textcolor)
+        self.meter3.setLabelColor(0, t_textcolor)
+        self.meter4.setLabelColor(0, t_textcolor)
  
 def CreateApplet(parent):
     return ATImon(parent)
